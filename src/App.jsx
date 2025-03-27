7c6b6405d7b3fa372953ffc06cc9f12be62fd0f4
yuliiapodlutskaia <yulechka-ya-2015@mail.ru>
2025-03-27 20:27:29 +0800

    prettier added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './app/App.css'
-import Admin from "./pages/AdminsPage/Admin";
-import Auth from "./modules/auth/Auth";
-import Registration from "./modules/registration/registration";
-
+import Admin from './pages/AdminsPage/Admin'
+import Auth from './modules/auth/Auth'
+import Registration from './modules/registration/registration'
 
 function App() {
-
-  return (
-    <Router>
-        <Routes>
-            <Route path="/" element={<Auth/>} />
-            <Route path="registration" element={<Registration/>}></Route>
-        </Routes>
-    </Router>
-
-  )
-
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Auth />} />
+                <Route path="registration" element={<Registration />}></Route>
+            </Routes>
+        </Router>
+    )
 }
-
 
 export default App
